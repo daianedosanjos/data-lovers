a9eeb2a96ac1fbbea67b355938030f58a8c28a34
Geice Sousa Pinho <geicesousa@outlook.com>
2023-02-24 17:39:24 -0300

    css de jogos com font size ajustado

--- src/quiz.js
+++ src/quiz.js
@@ -2,8 +2,8 @@
 //style.backgroundColor = "rgba(0, 228, 0, 0.459)"
 //else backgroundColor = "rgba(255, 0, 0, 0.459)"
 
-const respostaCorreta = document.querySelectorAll(".quizRespostas");
-console.log(respostaCorreta)
-respostaCorreta.addEventListener("input", ()=>{
-  console.log("funcionou")
-})
+// const respostaCorreta = document.querySelectorAll(".quizRespostas");
+// console.log(respostaCorreta)
+// respostaCorreta.addEventListener("input", ()=>{
+//   console.log("funcionou")
+// })
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,85 +1,154 @@
-import {alphabeticalOrder, searchByName, filter} from '../src/data.js';
+import {filter, alphabeticalOrder, searchByName, calculatePercentage} from "../src/data.js";
 
 const rick = {
-  "name": "Rick Sanchez",
-  "status": "Alive",
-  "species": "Human",
-  "gender": "Male", 
-}
+  name: "Rick Sanchez",
+  status: "Alive",
+  species: "Human",
+  gender: "Male",
+};
 
 const chris = {
-  "name": "Chris",
-  "status": "Dead",
-  "species": "Alien",
-  "gender": "unknown",
-}
+  name: "Chris",
+  status: "Dead",
+  species: "Alien",
+  gender: "unknown",
+};
 
 const fleeb = {
-  "name": "Fleeb",
-  "status": "unknown",
-  "species": "Alien",
-  "gender": "unknown",
-}
+  name: "Fleeb",
+  status: "unknown",
+  species: "Alien",
+  gender: "unknown",
+};
 
 const jackie = {
-  "name": "Jackie",
-  "status": "Alive",
-  "species": "Alien",
-  "gender": "Female",
-}
-
+  name: "Jackie",
+  status: "Alive",
+  species: "Alien",
+  gender: "Female",
+};
 const beebo = {
-  "name": "Beebo",
-  "status": "Dead",
-  "species": "Alien",
-  "gender": "Male", 
- 
-}
+  name: "Beebo",
+  status: "Dead",
+  species: "Alien",
+  gender: "Male",
+};
 
-const arrayTest = [rick, chris, fleeb, jackie, beebo ]
+const arrayTest = [rick, chris, fleeb, jackie, beebo];
 
-describe ('ordenar lista personagens', () => {
-  it('is a function', () => {
-    expect(typeof alphabeticalOrder).toBe('function');//toBe(valor) testa se o valor passado é idêntico ao esperado em valor e tipo
-  })
-
-  it('ordenar de a-z', () =>{
-    const valueAz = "a-z"
-
-    expect(alphabeticalOrder(valueAz, arrayTest)).toEqual([arrayTest[4], arrayTest[1], arrayTest[2], arrayTest[3], arrayTest[0]])
-  })
-  //A função . toEqual(valor) testa recursivamente cada valor do objeto ou array.
-
-  it('ordenar de z-a', () =>{
-    const valueZa = "z-a"
-
-    expect(alphabeticalOrder(valueZa, arrayTest)).toEqual([arrayTest[0], arrayTest[3], arrayTest[2], arrayTest[1], arrayTest[4]])
-  })
-})
-
-describe('filtrar por nome', () => {
-  it('is a function', () => {
-    expect(typeof searchByName).toBe('function')
+describe("filtrar por key", () => {
+  it("is a function", () => {
+    expect(typeof filter).toBe("function");
   });
 
-  it('retornar', () => {
-    expect(searchByName(arrayTest, "rick")).toEqual([arrayTest[0]]);
-  })
+  it("filtrar por genero feminino", () => {
+    const gender = filter(arrayTest, "female", "gender");
+    expect(gender.length).toEqual(1);
+    expect(gender).toEqual([arrayTest[3]]);
+  });
+
+  it("filtrar por genero masculino", () => {
+    const gender = filter(arrayTest, "male", "gender");
+    expect(gender.length).toEqual(2);
+    expect(gender).toEqual([arrayTest[0], arrayTest[4]]);
+  });
+
+  it("filtrar por genero indefinido", () => {
+    const gender = filter(arrayTest, "unknown", "gender");
+    expect(gender.length).toEqual(2);
+    expect(gender).toEqual([arrayTest[1], arrayTest[2]]);
+  });
+
+  it("filtrar por status indefinido", () => {
+    const status = filter(arrayTest, "unknown", "status");
+    expect(status.length).toEqual(1);
+    expect(status).toEqual([arrayTest[2]]);
+  });
+
+  it("filtrar por status morto", () => {
+    const status = filter(arrayTest, "dead", "status");
+    expect(status.length).toEqual(2);
+    expect(status).toEqual([arrayTest[1], arrayTest[4]]);
+  });
+
+  it("filtrar por status vivo", () => {
+    const status = filter(arrayTest, "alive", "status");
+    expect(status.length).toEqual(2);
+    expect(status).toEqual([arrayTest[0], arrayTest[3]]);
+  });
 });
 
-describe('buscar por status', () => {
-  it('is a function', () => {
-    expect(typeof filter).toBe('function')
+describe("ordenar lista personagens", () => {
+  it("is a function", () => {
+    expect(typeof alphabeticalOrder).toBe("function"); //toBe(valor) testa se o valor passado é idêntico ao esperado em valor e tipo
   });
 
-  it('retornar personagem por status filtrado', () => {
-    const especie1 = "Alive"
-    const especie2 = "Dead"
-    const especie3 = "unknown"
+  it("ordenar de a-z", () => {
+    const valueAz = "a-z";
+    expect(alphabeticalOrder(valueAz, arrayTest)).toEqual([
+      arrayTest[4],
+      arrayTest[1],
+      arrayTest[2],
+      arrayTest[3],
+      arrayTest[0],
+    ]);
+  });
+  //A função .toEqual(valor) testa recursivamente cada valor do objeto ou array.
 
+  it("ordenar de z-a", () => {
+    const valueZa = "z-a";
+    expect(alphabeticalOrder(valueZa, arrayTest)).toEqual([
+      arrayTest[0],
+      arrayTest[3],
+      arrayTest[2],
+      arrayTest[1],
+      arrayTest[4],
+    ]);
+  });
+});
 
-    expect(filter(especie1, arrayTest)).toEqual([arrayTest[3]]);
-    expect(filter(especie2, arrayTest)).toEqual([arrayTest[2]]);
-    expect(filter(especie3, arrayTest)).toEqual([arrayTest[3]]);
-  })
+describe("filtrar por nome", () => {
+  it("is a function", () => {
+    expect(typeof searchByName).toBe("function");
+  });
+
+  it("retornar", () => {
+    expect(searchByName(arrayTest, "rick")).toEqual([arrayTest[0]]);
+  });
+});
+
+describe("calcular porcentagem", () => {
+  it("is a function", () => {
+    expect(typeof calculatePercentage).toBe("function");
+  });
+
+  it("calcular porcentagem dos femininos", () => {
+    const porcentagem = calculatePercentage(arrayTest.length, 1);
+    expect(porcentagem).toEqual(20);
+  });
+
+  it("calcular porcentagem dos masculinos", () => {
+    const porcentagem = calculatePercentage(arrayTest.length, 2);
+    expect(porcentagem).toEqual(40);
+  });
+
+  it("calcular porcentagem dos generos indefinidos", () => {
+    const porcentagem = calculatePercentage(arrayTest.length, 2);
+    expect(porcentagem).toEqual(40);
+  });
+
+  it("calcular porcentagem dos vivos", () => {
+    const porcentagem = calculatePercentage(arrayTest.length, 2);
+    expect(porcentagem).toEqual(40);
+  });
+
+  it("calcular porcentagem dos morto", () => {
+    const porcentagem = calculatePercentage(arrayTest.length, 2);
+    expect(porcentagem).toEqual(40);
+  });
+
+  it("calcular porcentagem dos status indefinidos", () => {
+    const porcentagem = calculatePercentage(arrayTest.length, 1);
+    expect(porcentagem).toEqual(20);
+  });
 });
