6c4ee498b4bd86688e3e2e75dcb66e10a394ad62
daiane-dosanjos <dayanjos01@gmail.com>
2023-03-08 01:25:00 -0300

    final

--- src/quiz.js
+++ src/quiz.js
@@ -2,21 +2,50 @@ import  { calculatePercentage } from "./data.js";
 
 const formQuiz = document.querySelector("#quiz");
 
-formQuiz.addEventListener("click", (e)=>{
+formQuiz.addEventListener("click", (e) => {
   const elemento = e.target;
   const ehRadio = elemento.type === "radio";
   const ehCorreta = ehRadio && elemento.value === "correta";
 
-  if(ehRadio){
-    elemento.parentNode.parentNode.dataset.score = ehCorreta;
-    //fazer selecionar apenas uma vez
+  if (ehRadio) {
+    const box = elemento.parentNode.parentNode;
+
+    box.dataset.score = ehCorreta;
+    box.classList.add("desabilita");
   }
-})
+});
 
-
-// export const calculatePercentage = (dataList, dataListFiltered)=> {    
-//   return parseInt((dataListFiltered*100)/dataList)
-// }
-    
-// const percentage = calculatePercentage(characters.length, filteredSearch.length);
-//   percentageReturn.innerHTML = `Essa lista contém ${filteredSearch.length} personagens, que equivale á ${percentage}% dos personagens totais`; 
+const enviaQuiz = document.querySelector("#btn");
+enviaQuiz.addEventListener("click", function calculaAcertos (){
+  const divQuiz = document.querySelectorAll(".divQuiz");
+  const mensage = document.querySelector(".mensage");
+  const jogarNovamente = document.querySelector(".jogarNovamente");
+  const qtdDeCorretas = document.querySelectorAll("input[type=radio][value=correta]:checked");
+  const percentage = calculatePercentage(divQuiz.length,qtdDeCorretas.length );
+  
+  if(percentage >= 70){
+    mensage.innerHTML = `Parabéns!!! Você é um super fã!!! Acertou ${percentage}% das perguntas.`
+    jogarNovamente.style.display = "block";
+    enviaQuiz.remove()
+  } 
+  
+  if(percentage < 70){
+    mensage.innerHTML = `Parabéns! Você acertou ${percentage}% das perguntas. Já pode dizer que é fã...`
+    jogarNovamente.style.display = "block";
+    enviaQuiz.remove()
+  } 
+  
+  if(percentage <= 50){
+    mensage.innerHTML = `Você acertou ${percentage}% das perguntas. Dá pra melhorar...`
+    jogarNovamente.style.display = "block";
+    enviaQuiz.remove()
+  } 
+  
+  if(percentage < 20){
+    mensage.innerHTML = `Você acertou ${percentage}% das perguntas. Parece que alguém precisa assistir mais epsódios...`
+    jogarNovamente.style.display = "block";
+    enviaQuiz.remove()
+  }
+ 
+}
+);
